b5f39a93cab180d1535edd462d116978af5cfb5c
lefrancis <luis.francis44@yahoo.com>
2022-12-15 15:31:30 -0500

    Modified comments to reflect the authorship of ALL the code contained in these files solely belongs to Sandro Tskitishvili, Luis Francis just added the comments and created the repository and assisted with pull/push to github.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// All code written for this game was written by Sandro Tskitishvili
+// All code written for this game were written solely by Sandro Tskitishvili.
 let x = Math.floor(Math.random() * 100) +1;
 let y = Math.floor(Math.random() * 100) +1;
 let z = Math.floor(Math.random() * 100) +1;
@@ -56,5 +56,4 @@ hint.addEventListener("click", function () {
 
 reset.addEventListener("click", function () {
 location.reload()})
-
-// All code written for this game was written by Sandro Tskitishvili
+// All code written for this game were written solely by Sandro Tskitishvili.
