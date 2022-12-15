e017d45d530a3a7972e421bdb32667b1cacc03b5
Sandrieli13 <tskitishvilisandro@gmail.com>
2022-12-15 14:52:17 -0500

    6565

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let x = Math.floor(Math.random() * 100) +1;
 let y = Math.floor(Math.random() * 100) +1;
 let z = Math.floor(Math.random() * 100) +1;
-const mynum = document.querySelector("div")
+const mynum = document.querySelector("#gspan")
 let click = 0
 const input = document.querySelector('input')
 const h1 = document.querySelector('h1')
@@ -9,35 +9,47 @@ const h2 = document.querySelector('h2')
 const Guess = document.querySelector("#guess");
 const hint = document.querySelector("#hint")
 const reset = document.querySelector("#reset")
-const hints = document.querySelector("p")
+const hints = document.querySelector("#hspan")
 Guess.addEventListener("click", function () {
-	click++;
-	if (click <= 5) {
-		if (input.value == x) {
-			h1.innerHTML = "You Win";
-			h2.innerHTML = "";
-		} else {
-			if (input.value > x) {
-				h1.innerHTML = "Go Lower";
-			} else {
-				h1.innerHTML = "Go Higher";
-			}
-			if (Math.abs(x - input.value) < 10) {
-				if (Math.abs(x - input.value) < 5) {
-					h2.innerHTML = "You are very close";
-				} else {
-					h2.innerHTML = "You are close";
-				}
-			} else {
-				if (Math.abs(x - input.value) > 50) {
-					h2.innerHTML = "You are very far";
-				} else {
-					h2.innerHTML = "You are far";
-				}
-			}
-		}
-	} else {
-		h1.innerHTML = "You Lose";
-		h2.innerHTML = "";
-	}
-});
+click++ 
+if(click<=5){
+    let j=input.value
+    mynum.innerHTML +=j+", "  
+if(input.value==x){
+    h1.innerHTML = "You Win"
+    h2.innerHTML = input.value +" is the number!"
+    h1.style.color = "green"
+  }else{if(input.value>x){
+    h1.innerHTML = "Go Lower"
+    h1.style.color = "teal"
+  }else{
+    h1.innerHTML = "Go Higher"
+    h1.style.color = "teal"
+  }
+  if(Math.abs(x-input.value)<15){
+    if(Math.abs(x-input.value)<5){
+        h2.innerHTML= "Hot"
+        h2.style.color = "orange"
+    }else{
+        h2.innerHTML= "Warm"
+        h2.style.color = "yellow"
+    }
+  }else{if(Math.abs(x-input.value)>50){
+    h2.innerHTML= "Freezing"
+    h2.style.color = "skyblue"
+  }else{
+    h2.innerHTML= "Cold"
+    h2.style.color = "blue"
+  }}}
+}else{
+    h1.innerHTML = "You Lose"
+    h2.innerHTML = ""
+    h1.style.color = "orangered"
+}});
+
+hint.addEventListener("click", function () {
+    hints.innerHTML = y +", "+ x +", "+ z
+})
+
+reset.addEventListener("click", function () {
+location.reload()})
